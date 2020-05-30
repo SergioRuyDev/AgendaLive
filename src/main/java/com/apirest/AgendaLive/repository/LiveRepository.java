7ff2d2086897a4c7de6460ae40de3536ae217ee5
Sergio Ruy <sergio_ruy@hotmail.com>
2020-05-30 17:37:16 +0400

    Initial commit

---
+++ src/main/java/com/apirest/AgendaLive/repository/LiveRepository.java
@@ -0,0 +1,15 @@
+package com.apirest.AgendaLive.repository;
+
+import com.apirest.AgendaLive.document.LiveDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.time.LocalDateTime;
+
+public interface LiveRepository extends MongoRepository<LiveDocument, String> {
+
+    Page<LiveDocument> findByLiveDateAfterOrderByLiveDateAsc(LocalDateTime date, Pageable pageable);
+    Page<LiveDocument> findByLiveDateBeforeOrderByLiveDateDesc(LocalDateTime date, Pageable pageable);
+
+}
